375ddd8cbd54c2e2026bf46a68787acb347f053b
parthtandoncs27-create <parth.tandon.cs27@iilm.edu>
2026-03-24 09:40:49 +0530

    Create Palindromic array using loops and recursion.java

---
+++ Palindromic array using loops and recursion.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static boolean isPerfect(int[] arr) {
+        // // code here
+        int i = 0;
+        int j = arr.length - 1;
+        // while(i > j) {
+        //     if(arr[i] != arr[j]) {
+        //         return false;
+        //     }
+        //     i++;
+        //     j--;
+        // }
+        // return true;
+        return check(arr,i,j);
+    }
+    static boolean check(int []arr,int i,int j) {
+        if (i>=j) {
+            return true;
+        }
+        if (arr[i] != arr[j]) {
+            return false;
+        }
+        return check(arr,i+1,j-1);
+    }
+}
