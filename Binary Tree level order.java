40b8ff9363cc39cf7a655338dbdc54b6b0c19fd3
parthtandoncs27-create <parth.tandon.cs27@iilm.edu>
2026-04-16 10:19:14 +0530

    Create Binary Tree level order.java

---
+++ Binary Tree level order.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> r = new ArrayList<>();
+        if(root == null) {
+            return r;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()) {
+            int s = q.size();
+            List<Integer> l = new ArrayList<>();
+            for(int i = 0;i < s;i++)  {
+                TreeNode n = q.poll();
+                l.add(n.val);
+                if(n.left != null) q.add(n.left);
+                if(n.right != null) q.add(n.right);
+            }
+            r.add(l);
+        }
+        return r;
+    }
+}
